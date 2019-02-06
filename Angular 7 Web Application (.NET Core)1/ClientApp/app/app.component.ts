072a7012c8185aa0f5007244657b4d774f067251
lluis cantón callau <lluis.cantoncallau@altran.com>
2019-02-06 14:45:39 +0100

    Initial Commit

---
+++ Angular 7 Web Application (.NET Core)1/ClientApp/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Bienvenidos a Angular';
+    subtitle = '.NET Core + Angular CLI v7 + Bootstrap & FontAwesome + Swagger Template';
+}
